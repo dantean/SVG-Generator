46985b9606cec67b1f163a0c228315fe82d7c169
Joseph Monticelli <jdmonticelli@gmail.com>
2024-05-05 19:53:24 -0400

    updated index.js to use 'import' ES6 code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const fs = require("fs");
-const Circle = require("./lib/circle");
-const Square = require("./lib/square");
-const Triangle = require("./lib/triangle");
+import fs from 'fs';
+import Circle from './lib/circle';
+import Square from './lib/square';
+import Triangle from './lib/triangle';
 import inquirer from 'inquirer';
 
 const questions = [
@@ -31,20 +31,31 @@ const questions = [
 inquirer.prompt(questions)
 .then(data => {
     if(data.shape === "circle") {
-        const circle = new Circle(data.logoText, data.textColor, data.shapeColor)
+        const circle = new Circle(data.logoText, data.textColor, data.shapeColor);
         fs.writeFile("./examples/circle.svg", circle.render(), err => {
-            console.log("Logo Successfully Created!")
-        })
-    }else if(data.shape === "square") {
-        const square = new Square(data.logoText, data.textColor, data.shapeColor)
+            if (err) {
+                console.error("Error creating file:", err);
+            } else {
+                console.log("Logo Successfully Created!");
+            }
+        });
+    } else if(data.shape === "square") {
+        const square = new Square(data.logoText, data.textColor, data.shapeColor);
         fs.writeFile("./examples/square.svg", square.render(), err => {
-            console.log("Logo Successfully Created!")
-        })
-
-    }else {
-        const triangle = new Triangle(data.logoText, data.textColor, data.shapeColor)
+            if (err) {
+                console.error("Error creating file:", err);
+            } else {
+                console.log("Logo Successfully Created!");
+            }
+        });
+    } else {
+        const triangle = new Triangle(data.logoText, data.textColor, data.shapeColor);
         fs.writeFile("./examples/triangle.svg", triangle.render(), err => {
-            console.log("Logo Successfully Created!")
-        })
+            if (err) {
+                console.error("Error creating file:", err);
+            } else {
+                console.log("Logo Successfully Created!");
+            }
+        });
     }
-})
+});
